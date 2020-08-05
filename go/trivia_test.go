0e54f79640c5e21df3c6a848014276fef1993914
Cassondra Foesch <puellanivis@gmail.com>
2020-08-05 13:27:27 +0000

    mob next [ci-skip]

--- go/trivia_test.go
+++ go/trivia_test.go
@@ -2,35 +2,53 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
 	"math/rand"
 	"testing"
 )
 
+var golden []byte
+
+func init() {
+	var err error
+
+	golden, err = ioutil.ReadFile("golden.txt")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func TestGame(t *testing.T) {
+	buf := new(bytes.Buffer)
+	output = buf
 
-	output = new(bytes.Buffer)
+	for i := int64(0); i < 256; i++ {
+		notAWinner := false
 
-	notAWinner := false
+		game := NewGame()
 
-	game := NewGame()
+		game.Add("Chet")
+		game.Add("Pat")
+		game.Add("Sue")
 
-	game.Add("Chet")
-	game.Add("Pat")
-	game.Add("Sue")
+		rand.Seed(i)
 
-	rand.Seed(42)
+		for {
+			game.Roll(rand.Intn(5) + 1)
 
-	for {
-		game.Roll(rand.Intn(5) + 1)
+			if rand.Intn(9) == 7 {
+				notAWinner = game.WrongAnswer()
+			} else {
+				notAWinner = game.WasCorrectlyAnswered()
+			}
 
-		if rand.Intn(9) == 7 {
-			notAWinner = game.WrongAnswer()
-		} else {
-			notAWinner = game.WasCorrectlyAnswered()
+			if !notAWinner {
+				break
+			}
 		}
+	}
 
-		if !notAWinner {
-			break
-		}
+	if !bytes.Equal(buf.Bytes(), golden) {
+		t.Fatal("output does not match golden")
 	}
 }
